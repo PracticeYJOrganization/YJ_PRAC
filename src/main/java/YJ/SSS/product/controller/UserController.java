45d56f31fc259c3e7cba20650420fd3ea4e8beba
YeomSB <stanley0927@naver.com>
2024-03-09 18:01:13 +0900

    feat : asd 기능 추가

--- src/main/java/YJ/SSS/product/controller/UserController.java
+++ src/main/java/YJ/SSS/product/controller/UserController.java
@@ -1,13 +1,17 @@
 package YJ.SSS.product.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @PostMapping("/bye")
+    @GetMapping("/bye")
     public String bye(){
         return "bye bye bye";
+    }
+    @GetMapping("/asd")
+    public String asd(){
+        return "asdasd";
     }
 }
